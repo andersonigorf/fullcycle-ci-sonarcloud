d1b13d42d3f278963794284619ab2184f1533ece
Anderson Igor <andersonigorf@gmail.com>
2021-10-26 17:43:55 -0300

    ci: verificar se o quality gate do sonar cloud está funcionando

--- src/math.test.js
+++ src/math.test.js
@@ -4,6 +4,14 @@ test("add 1 + 2 to be equal 3", () => {
     expect(math.add(1, 2)).toBe(3);
 });
 
+test("add 10 + 10 to be equal 20", () => {
+    expect(math.add(10, 10)).toBe(20);
+});
+
 test("multiply 1 * 5 to be equal 5", () => {
     expect(math.multiply(1, 5)).toBe(5);
+});
+
+test("multiply 100 * 0 to be equal 0", () => {
+    expect(math.multiply(100, 0)).toBe(0);
 });
